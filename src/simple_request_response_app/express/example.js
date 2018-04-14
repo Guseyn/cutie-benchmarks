59ad8067b047b6b8cde27faef9b1138bcb270862
gismayilov <gismayilov@griddynamics.com>
2018-04-14 13:06:04 +0300

    add some examples for some frameworks

--- src/simple_request_response_app/express/example.js
+++ src/simple_request_response_app/express/example.js
@@ -1,3 +1,26 @@
 /* 
   Example of simple request-response app on express framework
 */
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express(); 
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+
+  const { headers, method, url } = req;
+  const body = req.body;
+  const content = {headers, method, url, body};
+  res.status(200).send(`content is delivered => ${content}`);
+
+  // memory
+  const used = process.memoryUsage();
+  for (let key in used) {
+    console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
+  }
+});
+ 
+app.listen(4202);
